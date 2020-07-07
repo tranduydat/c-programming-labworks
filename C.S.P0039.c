ddc336c3bb1e9a766ba7babf51af07d825725f88
Dat Tran <trduydat@gmail.com>
2020-07-07 07:54:53 +0700

    Init Date Validator

--- C.S.P0039.c
+++ C.S.P0039.c
@@ -1,12 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Date
+{
+    int day;
+    int month;
+    int year;
+} Date;
+
+/*
+ *  Function
+ *  To get integer from standard input while check its validity
+ */
+int getIntInput(int min, int max)
+{
+    int userInput;
+    short isValid;
+    char enterChar;
+
+    // check validity of input
+    while (1)
+    {
+        isValid = scanf("%d%c", &userInput, &enterChar);
+        fflush(stdin);
+
+        /* input should be integer number
+         * and end line with enter character */
+        if (isValid != 2 || enterChar != '\n')
+            printf("Invalid input. Please enter an integer number in range of %d to %d.\nTry again: ", min, max);
+        else if (userInput < min || userInput > max)
+            printf("Input is Out of Range. Please enter a valid input in range of %d to %d.\nTry again: ", min, max);
+        else
+            return userInput;
+    }
+}
+
+/*
+ *  Function
+ *  To get 2 characters from standard input while check its validity
+ */
+void getCharInput(char *c1, char *c2)
+{
+    short isValid;
+    char enterChar;
+
+    do
+    {
+        printf("Enter 2 character: ");
+        isValid = scanf("%c%c%c", *(&c1), *(&c2), &enterChar);
+        fflush(stdin);
+
+        // 65 - 90 97 - 122
+        if (isValid != 3 || enterChar != '\n')
+            printf("Invalid input. Please enter just only 2 character.\n");
+        else
+            break;
+    } while (isValid != 3 || enterChar != '\n');
+}
+
 /*
  *  Function
  *  Check input year (as parameter) either is leap year or not
  *  Return 1 if it is, unless return 0
  */
-int checkLeapYear(int year) {
+int checkLeapYear(int year)
+{
     /* A leap year is a year that divisible by 400 or divisible by 4
      * and indivisible by 100. If it is a leap year,
      * then return 1, otherwise return 0 */
@@ -35,33 +93,34 @@ void getDateInfo(int *day, int *month, int *year)
  *  Function
  *  Calculating that type of dat which user inputted does it exist or not
  */
-void processDateInfo(int day, int month, int year) {
-    char errorMsg[] = "Day, month, year you entered is invalid\n";
+void processDateInfo(int day, int month, int year)
+{
+    char errorMsg[] = "\nDay, month, year you entered is invalid\n";
+    char successMsg[] = "\nYou entered a valid date: ";
 
     if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
     {
         if (day <= 31)
-            printf("\n%d-%d-%d\n", day, month, year);
+            printf("%s%d-%d-%d\n", successMsg, day, month, year);
         else
             printf("%s", errorMsg);
     }
-    else if  (month == 2)
+    else if (month == 2)
     {
         if (checkLeapYear(year))
             if (day <= 29)
-                printf("\n%d-%d-%d\n", day, month, year);
+                printf("%s%d-%d-%d\n", successMsg, day, month, year);
             else
                 printf("%s", errorMsg);
+        else if (day <= 28)
+            printf("%s%d-%d-%d\n", successMsg, day, month, year);
         else
-            if (day <= 28)
-                printf("\n%d-%d-%d\n", day, month, year);
-            else
-                printf("%s", errorMsg);
+            printf("%s", errorMsg);
     }
     else
     {
         if (day <= 30)
-            printf("\n%d-%d-%d\n", day, month, year);
+            printf("%s%d-%d-%d\n", successMsg, day, month, year);
         else
             printf("%s", errorMsg);
     }
@@ -71,8 +130,9 @@ void processDateInfo(int day, int month, int year) {
  *  Function
  *  To print out the menu to the console
  */
-void getUserChoice(int *userChoice) {
-    printf("MENU\n-------------------------------\n\n");
+void getUserChoice(int *userChoice)
+{
+    printf("MENU\n---------------------------\n\n");
     printf("1- Processing date data\n");
     printf("2- Character data\n");
     printf("3- Quit\n");
@@ -83,40 +143,61 @@ void getUserChoice(int *userChoice) {
 
 /*
  *  Function
- *  To get integer input from user while check its validity
+ *  Printing output in descending orders of ASCII codes of all characters
+ *  between the 2 inputted characters (inclusively) to standard output
  */
-int getIntInput(int max, int min) {
-    int userInput;
-    short isValid;
-    char enterChar;
+void printCharactersData(char a, char b)
+{
+    int i;
 
-    // check validity of input
-    while (1) {
-        isValid = scanf("%d%c", &userInput, &enterChar);
-        fflush(stdin);
-
-        /* input should be integer number
-         * and end line with enter character */
-        if (isValid != 2 || enterChar != '\n')
-            printf("Invalid input. Please enter an integer number in range of %d to %d", min, max);
-        else
-            return userInput;
-    }
+    printf("Converting characters to ASCII Code:\n");
+    /* to print out characters and
+     * its corresponding ASCII terms
+     * in Descending order */
+    if (a < b)
+        for (i = b; i >= a; i--)
+            printf("  %c:%d\n", i, i);
+    else
+        for (i = a; i >= b; i--)
+            printf("  %c:%d\n", i, i);
 }
 
-void getCharInput();
+/*
+ *  Function
+ *  To rerun program as soon as it be invoked
+ */
+void rerunProgram()
+{
+    fflush(stdin);
+    printf("\n");
+    main();
+}
 
-int main() {
+int main()
+{
     int userChoice;
+    int day, month, year;
+    char c1, c2;
+    Date date;
 
+    // prompt user to get their choice then performing corresponding function
     getUserChoice(&userChoice);
-    switch (userChoice) {
-        case 1:
-            getDateInfo(&day, &month, &year);
-            processDateForm(day, month, year);
-            break;
-        case 2:
+
+    switch (userChoice)
+    {
+    case 1:
+        getDateInfo(&day, &month, &year);
+        processDateInfo(day, month, year);
+        break;
+    case 2:
+        getCharInput(&c1, &c2);
+        printCharactersData(c1, c2);
+        break;
+    case 3:
+        exit(0);
     }
+
+    rerunProgram();
 
     return 0;
 }
